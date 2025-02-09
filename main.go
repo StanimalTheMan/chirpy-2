136ea015ef1bf8b665923984b563cbe8dbae8298
Stan Choi <sjc591@nyu.edu>
2025-02-09 00:45:06 -0500

    init

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+	serveMux := http.ServeMux{}
+	server := http.Server{
+		Handler: &serveMux,
+		Addr:    ":8080",
+	}
+
+	server.ListenAndServe()
+}
