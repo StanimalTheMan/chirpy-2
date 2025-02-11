059c92a49dbc49dad3a7ce93b89462e2355ada61
Stan Choi <sjc591@nyu.edu>
2025-02-11 12:11:19 -0500

    Refactor code

--- main.go
+++ main.go
@@ -22,9 +22,10 @@ func main() {
 	mux.Handle("/app/", fsHandler)
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerPost)
 
 	server := &http.Server{
 		Addr:    ":" + port,
