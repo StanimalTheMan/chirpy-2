35f5ade53f03936ba1ecbae0803625487347d9a0
Stan Choi <sjc591@nyu.edu>
2025-02-12 19:22:21 -0500

    Refactor code

--- reset.go
+++ reset.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"context"
-	"log"
 	"net/http"
 )
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	if cfg.env != "dev" {
 		w.WriteHeader(http.StatusForbidden)
-	} else {
-		err := cfg.db.DeleteUsers(context.Background())
-		if err != nil {
-			log.Fatalf("There was an error deleting all users: %s", err)
-		}
-		cfg.fileserverHits.Store(0)
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hits reset to 0"))
+		w.Write([]byte("Reset is only allowed in dev environment."))
+		return
 	}
+	cfg.fileserverHits.Store(0)
+	cfg.db.Reset(context.Background())
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0 and database reset to initial state."))
+
 }
